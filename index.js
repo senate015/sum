10d9bdfa27e1bbd1d788a509c5ba5d7b77469ab2
Andy Hughes <senate014@gmail.com>
2021-01-26 17:33:52 +0000

    added in the ability to sum 3 values

--- index.js
+++ index.js
@@ -2,3 +2,9 @@ const operations = require('./operations.js')
 
 const result = operations.sum(2, 40)
 console.log("The result is: " + result)
+
+console.log(operations.sum(operations.sum(1, 2), 3))
+
+console.log(operations.sum(7, 2, 3))
+
+console.log(operations.sum(1))
--- operations.js
+++ operations.js
@@ -1,5 +1,5 @@
-function sum (a,b) {
-    return a+b
+function sum (a =0 , b = 0, c = 0) {
+    return a+b+c 
 }
 
 module.exports = {
